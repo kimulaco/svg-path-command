01671076176cb49b143c9f2e8da15c96dad39b4d
kimulaco <kimulaco@gmail.com>
2023-04-20 07:48:24 +0900

    Add Command.unmarshall()

--- lib/Command/Command.spec.ts
+++ lib/Command/Command.spec.ts
@@ -20,4 +20,11 @@ describe('Command', () => {
     expect(result).toBe(undefined)
     expect(command.result).toBe(undefined)
   })
+
+  test('throw set invalid params', async () => {
+    expect(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      command.params = 100 as any
+    }).toThrow('Invalid params array')
+  })
 })
