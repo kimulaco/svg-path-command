28cf5c607b59fc04dfd558d81f8b4526bf4bc2aa
kimulaco <kimulaco@gmail.com>
2023-04-20 09:23:14 +0900

    Update ACommand

--- lib/Command/A.ts
+++ lib/Command/A.ts
@@ -1,5 +1,6 @@
 import { Command } from './Command'
 import type { CommandParseResult } from './Command'
+import { ParserError } from '../ParserError'
 
 export type AbsoluteACommandParseResult = CommandParseResult<
   'rx' | 'ry' | 'largeArcFlag' | 'sweepFlag' | 'x' | 'y'
@@ -29,8 +30,40 @@ export class AbsoluteACommand extends Command<
     return result
   }
 
-  validate(): boolean {
-    return this.params.length === PARAM_LENGTH_A
+  unmarshall(): number[] {
+    if (!this.validateResult(this.result)) {
+      throw new ParserError('Invalid result object')
+    }
+
+    const result = { ...this.result }
+    const params = [
+      result.rx,
+      result.ry,
+      result.largeArcFlag,
+      result.sweepFlag,
+      result.x,
+      result.y,
+    ]
+    this.params = params
+
+    return params
+  }
+
+  validateParams(value: number[]): boolean {
+    return value.length === PARAM_LENGTH_A
+  }
+
+  validateResult(
+    value: AbsoluteACommandParseResult | undefined
+  ): value is AbsoluteACommandParseResult {
+    return (
+      typeof value?.rx === 'number' &&
+      typeof value?.ry === 'number' &&
+      typeof value?.largeArcFlag === 'number' &&
+      typeof value?.sweepFlag === 'number' &&
+      typeof value?.x === 'number' &&
+      typeof value?.y === 'number'
+    )
   }
 }
 
@@ -51,7 +84,39 @@ export class RelativeACommand extends Command<
     return result
   }
 
-  validate(): boolean {
-    return this.params.length === PARAM_LENGTH_A
+  unmarshall(): number[] {
+    if (!this.validateResult(this.result)) {
+      throw new ParserError('Invalid result object')
+    }
+
+    const result = { ...this.result }
+    const params = [
+      result.rx,
+      result.ry,
+      result.largeArcFlag,
+      result.sweepFlag,
+      result.dx,
+      result.dy,
+    ]
+    this.params = params
+
+    return params
+  }
+
+  validateParams(value: number[]): boolean {
+    return value.length === PARAM_LENGTH_A
+  }
+
+  validateResult(
+    value: RelativeACommandParseResult | undefined
+  ): value is RelativeACommandParseResult {
+    return (
+      typeof value?.rx === 'number' &&
+      typeof value?.ry === 'number' &&
+      typeof value?.largeArcFlag === 'number' &&
+      typeof value?.sweepFlag === 'number' &&
+      typeof value?.dx === 'number' &&
+      typeof value?.dy === 'number'
+    )
   }
 }
