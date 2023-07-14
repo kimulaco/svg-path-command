5a6633c46fb1bbc291d3e7078b6b4f712643e8a3
kimulaco <kimulaco@gmail.com>
2023-07-15 07:53:23 +0900

    Add typedoc

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,11 @@
+export {
+  PathCommand,
+  type CommandTypes,
+  type PathCommandParams,
+  type PathCommandValue,
+  type PathCommandSeparator,
+} from './PathCommand'
+
+export { PathCommandList } from './PathCommandList'
+
 export { parsePathCommandList as parse } from './parser'
