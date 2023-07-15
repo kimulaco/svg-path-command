ec385ac7f60d873ba9d07b61f2b0591b93f04b2e
kimulaco <kimulaco@gmail.com>
2023-07-16 08:38:18 +0900

    Fix code format

--- lib/PathCommand/utils/isValidCommandType.ts
+++ lib/PathCommand/utils/isValidCommandType.ts
@@ -1,7 +1,7 @@
 import { PATH_COMMAND_COMMANDS, type CommandTypes } from '../'
 
 export const isValidCommandType = (
-  command: string
+  command: string,
 ): command is CommandTypes => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return PATH_COMMAND_COMMANDS.includes(command as any)
